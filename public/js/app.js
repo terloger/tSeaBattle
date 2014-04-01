ff962fffbc40a20dc6a291119f58398afc45c075
ter <s.redkin@miralab.ru>
2014-04-01 22:34:42 +0400

    inprogress

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,35 @@
-var tSeaBattle = {};
+var DIRECTION = {
+		vertical: 'v',
+		horizontal: 'h'
+	};
+
+function GameField() {
+	this.dimension = {
+		x: 25,
+		y: 25
+	};
+}
+
+GameField.prototype.render = function(container) {
+	
+};
+
+function Ship() {
+	this.size = 1;
+	this.direction = DIRECTION.horizontal;
+	this.startPoint = {
+		x: 1,
+		y: 1
+	};
+}
+
+var tSeaBattle = (function() {
+	
+	return {
+		init: function() {
+			
+		}
+	}
+})();
+
+$(tSeaBattle.init);
